3a0550143e76f46ae7fe01490d75a94eaddaab95
turboc1208 <chip.cox@coxdogs.com>
2017-05-12 13:59:00 -0500

    removed test icons

--- motion_control.py
+++ motion_control.py
@@ -4,5 +4,55 @@ class motion_control(appapi.my_appapi):
 
   def initialize(self):
     # self.LOGLEVEL="DEBUG"
+    self.motion_devices= {"sensor.office_sensor_burglar_11_10":
+                             {"target":"input_boolean.officemotion",
+                              "on_value":8,
+                              "off_value":0,
+                              "delay":300},
+                          "binary_sensor.den_sensor_sensor_13_0":
+                             {"target":"input_boolean.denmotion",
+                              "on_value":"on",
+                              "off_value":"off",
+                              "delay":300},
+                          "binary_sensor.aeotec_zw074_multisensor_gen5_sensor_14_0":
+                             {"target":"input_boolean.upstairsmotion",
+                              "on_value":"on",
+                              "off_value":"off",
+                              "delay":300}}
+  
     self.log("motion_control App")
+    for s_motion in self.motion_devices:
+      self.log("setting state listener for {}".format(s_motion))
+      self.listen_state(self.motion_handler,s_motion)
+      self.log("current {} state = {} on_value={}".format(s_motion,self.get_state(s_motion),self.motion_devices[s_motion]["on_value"]))
+      if str(self.get_state(s_motion))==str(self.motion_devices[s_motion]["on_value"]):
+        self.log("{} already on, setting timeout listener".format(s_motion))
+        self.turn_on(self.motion_devices[s_motion]["target"])
+        #self.set_state(self.motion_devices[s_motion]["target"],attributes={"entity_picture":"/local/black_ink.png"})
+        self.listen_state(self.motion_timeout,s_motion,
+                          new=self.motion_devices[s_motion]["off_value"],
+                          duration=self.motion_devices[s_motion]["delay"])
+      else:
+        self.log("{} is off".format(s_motion))
+        self.turn_off(self.motion_devices[s_motion]["target"])
+        #self.set_state(self.motion_devices[s_motion]["target"],attributes={"entity_picture":"/local/yellow_ink.png"})
 
+        
+    
+  def motion_handler(self,entity, attribute, old, new, kwargs):
+    if new!=old:
+      self.log("Motion happened - {} - {}".format(entity,new))
+      if str(new)==str(self.motion_devices[entity]["on_value"]):
+        self.turn_on(self.motion_devices[entity]["target"])
+        self.log("registering delayed state listener for {}".format(entity))
+        self.listen_state(self.motion_timeout,entity,
+                          new=str(self.motion_devices[entity]["off_value"]),
+                          duration=self.motion_devices[entity]["delay"])
+
+  def motion_timeout(self,entity, attribute, old, new, kwargs):
+    self.log("timout occurred for {} - {}".format(entity,new))
+    if str(new)==str(self.motion_devices[entity]["off_value"]):
+      self.log("turning out light {}".format(self.motion_devices[entity]["target"]))
+      self.turn_off(self.motion_devices[entity]["target"])
+
+  
